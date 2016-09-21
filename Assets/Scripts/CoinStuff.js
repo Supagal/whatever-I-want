1c81b4f2f749b3e6c5b68048d2579223f5c1c0f7
Lauren Griffith <lgrif57@gmail.com>
2016-09-21 11:47:55 -0400

    cfhfghjf hjf jf

--- Assets/Scripts/CoinStuff.js
+++ Assets/Scripts/CoinStuff.js
@@ -1,8 +1,8 @@
-﻿#pragma strict
+﻿#pragma strict 
 
 public var points : int = 5;
 
-public var pickedUpBy: String = "Player";
+public var pickedUpBy: String = "ball";
 
 function OnTriggerEnter2D (other : Collider2D)
 {
@@ -10,7 +10,7 @@ function OnTriggerEnter2D (other : Collider2D)
 	if (other.CompareTag(pickedUpBy) )
 
 	{
-		Debug.Log("Coins! Worth " + points + " points!");
+		Debug.Log("WOW! Worth " + points + " points!");
 
 		Destroy(gameObject);
 
