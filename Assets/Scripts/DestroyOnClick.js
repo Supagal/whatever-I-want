992446ef5ead7f19e79b7df72c69007323072976
Lauren Griffith <lgrif57@gmail.com>
2016-09-30 12:07:18 -0400

    bomb falls click no more bomb

--- Assets/Scripts/DestroyOnClick.js
+++ Assets/Scripts/DestroyOnClick.js
@@ -3,7 +3,12 @@
 function OnMouseDown()
 
 { 
-	if(gameObject.transform.position.y >-2 && gameObject.transform.position.y >3)
+	if(gameObject.transform.position.y >-2 && gameObject.transform.position.y <3)
 
-	Destroy (gameObject);
+	{
+
+	GameData.numGrabbed++;
+	GameData.DisplayScore();
+	Destroy(gameObject);
+}
 }
